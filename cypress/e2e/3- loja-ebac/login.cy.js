e8de91b0509f9514bdaf4552db0c708cc09d5942
Thiago Gregorio <thiago.gregorio@4win.local>
2024-03-11 08:52:51 -0300

    modificação

--- cypress/e2e/3- loja-ebac/login.cy.js
+++ cypress/e2e/3- loja-ebac/login.cy.js
@@ -8,10 +8,11 @@ describe('Funcionalidade: Login',() =>{
     
     
 
-    it ('Deve fazer login com sucesso',()=>{
+    it ('Deve fazer login com sucesso',() => {
         cy.get('#username').type('thiago.teste@teste.com.br')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click ()
+
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, thiago.teste (não é thiago.teste? Sair)')
     }) 
 
@@ -22,13 +23,13 @@ it ('deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
     cy.get('#password').type('teste123')
     cy.get('.woocommerce-form > .button').click ()
 
-    //cy.get('.woocommerce-error'). should('contain', 'Endereço de e-mail desconhecido.')
-    cy.get('.woocommerce-error'). should('exist') 
-})
+    //cy.get('.woocommerce-error').should('contain','Endereço de e-mail desconhecido.')
+    cy.get('.woocommerce-error').should('exist') 
+});
 
 it ( 'Deve exibir uma mensagem de erro ao inserir senha invalida', () =>{
     cy.get('#username').type('thiago.teste@teste.com.br')
-    cy.get('#password').type('teste12')
+    cy.get('#password').type('teste123')
     cy.get('.woocommerce-form > .button').click ()
 
     cy.get('.woocommerce-error'). should('contain' , 'Erro: A senha fornecida para o e-mail thiago.teste@teste.com.br está incorreta. Perdeu a senha?')

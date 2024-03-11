1d8468cf391d034770d612c00de1cf1d4276f5ea
TGregorio11 <thiago.gregorio11@gmail.com>
2024-03-11 20:28:36 -0300

    mod

--- cypress/e2e/3- loja-ebac/login.cy.js
+++ cypress/e2e/3- loja-ebac/login.cy.js
@@ -2,32 +2,12 @@
 
 describe('Funcionalidade: Login', () => {
 
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    });
+it('Deve fazer login com sucesso', () => {
+    cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.get('#username').type('thiago.teste@teste.com.br')
+    cy.get('#password').type('teste123')
+    cy.get('.woocommerce-form > .button').click()
+    
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, thiago.teste (não é thiago.teste? Sair)')}    )}
 
-    it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('thiago.teste@teste.com.br')
-        cy.get('#password').type('teste123')
-        cy.get('.woocommerce-form > .button').click()
-
-       
-    });
-
-    it('deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('thiago@teste.com.br')
-        cy.get('#password').type('teste123')
-        cy.get('.woocommerce-form > .button').click()
-
-    });
-
-    it('Deve exibir uma mensagem de erro ao inserir senha invalida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('thiago.teste@teste.com.br')
-        cy.get('#password').type('teste123')
-        cy.get('.woocommerce-form > .button').click()
-
-    })
-
-
+)
